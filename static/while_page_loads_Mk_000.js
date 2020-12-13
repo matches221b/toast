64dace2bae02b72f25aff750dd90100dfb93af46
matches221b <barrettyoung@gmail.com>
2020-12-13 00:57:38 -0500

    Test 001
    Test 001

--- static/while_page_loads_Mk_000.js
+++
@@ -1,9 +0,0 @@
-// Javascript file to help display something while a page loads
-// https://smallenvelop.com/display-loading-icon-page-loads-completely/
-
-$(window).load(function() {
-// $(window).on(‘load’, function() {
-	// Animate loader off screen
-	$(".se-pre-con").fadeOut("slow");;
-});
-
--- static/while_page_loads_Mk_003.js
+++
@@ -1,5 +0,0 @@
-// https://stackoverflow.com/questions/10742665/jquery-whole-html-page-load-with-spinner
-
-$(window).on('load', function(){
-    $('#cover').fadeOut(200);
-})
